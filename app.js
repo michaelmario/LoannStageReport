e5b76e8fb1d1ebf3f7885803e7a2a66dca812fd8
michaelmario <marioachil@gmail.com>
2022-12-06 14:57:20 +0400

    deleted fichier.html

---
+++ app.js
@@ -0,0 +1,9 @@
+/*
+let message = document.querySelector('#textarea');
+let sentData = document.getElementById('sentData');
+let user = document.querySelector('#user');
+ let output = document.getElementById('output');
+sentData.addEventListener('click',function(){
+   output.style="display:block";
+   output.innerHTML = user.value + message.value ; 
+   });*/
